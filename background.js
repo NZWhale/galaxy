07683b4089ce7f8bf0d5b868dcfc222b79d2eba6
Little Whale <vladimirputin420@protonmail.com>
2020-06-01 18:10:03 +0300

    fixed

--- background.js
+++ background.js
@@ -1,23 +1,44 @@
+"use strict"
 const CANVAS        = document.getElementById('space');
       CANVAS.width  = window.innerWidth;
       CANVAS.height = window.innerHeight; 
-      ctx           = CANVAS.getContext('2d');
-      randomX       = Math.floor(Math.random() * CANVAS.width);
-      randomY       = Math.floor(Math.random() * CANVAS.height);
+const ctx           = CANVAS.getContext('2d');
+    //   randomX       = Math.floor(Math.random() * CANVAS.width);
+    //   randomY       = Math.floor(Math.random() * CANVAS.height);
 
-      //function which draws a star and returns its position
-let drawStar = function() {
-    arrayOfStars = new Array(); 
-    for (i = 0; i < 50; i++) {
-    randomX       = Math.floor(Math.random() * CANVAS.width);
-    randomY       = Math.floor(Math.random() * CANVAS.height);
-    starPosition  = [randomX, randomY]
-    ctx.fillStyle = 'red';
-    ctx.beginPath()
-    ctx.arc(randomX, randomY, 2, 0, Math.PI * 2);
-    ctx.fill()
-    ctx.closePath()
-    arrayOfStars.push(starPosition)
+    //function which generating stars positions
+let arrayOfStars = new Array(); 
+let generateStarsPositions = function() {
+    for (let i = 0; i < 50; i++) {
+        let randomX       = Math.floor(Math.random() * CANVAS.width);
+        let randomY       = Math.floor(Math.random() * CANVAS.height);
+        let starPosition = [randomX, randomY]
+        arrayOfStars.push(starPosition)
     }
-};
-    drawStar()
+} 
+    generateStarsPositions()
+
+    //function which draws a stars
+let drawStars = function() {
+    for (let i = 0; i < arrayOfStars.length; i++) {
+        ctx.fillStyle = 'red';
+        ctx.beginPath()
+        ctx.arc(arrayOfStars[i][0], arrayOfStars[i][1], 2, 0, Math.PI * 2);
+        ctx.fill()
+        ctx.closePath()
+    }
+}
+    drawStars()
+
+    //function which makes stars moveable
+let motionOfStars = function() {
+    while(true) {
+        
+        }
+        // arrayOfStars.forEach(() => {
+        // arrayOfStars[i++][i++];
+        // });
+    }
+}
+
+    motionOfStars()
