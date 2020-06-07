60c89e9629d50aaf96874b2e6ef17c274547e198
Little Whale <vladimirputin420@protonmail.com>
2020-06-07 15:35:02 +0300

    class was created

--- objects.js
+++ objects.js
@@ -5,33 +5,91 @@ const   CANVAS        = document.getElementById('space');
 const   ctx           = CANVAS.getContext('2d');
         ctx.fillStyle = 'red';
 
-let randomNumber = function() {
-        let number = Math.floor(Math.random() * 10) - 5 
-        return number
+let randomXVector = function() {
+        let xNumber = Math.floor(Math.random() * 10) - 5 
+        return xNumber
 }
-let num = randomNumber()
+let vectorX = randomXVector()
 
-let singleStar = {
-    generatePosition: function() {
-        let randomX       = Math.floor(Math.random() * CANVAS.width);
-        let randomY       = Math.floor(Math.random() * CANVAS.height);
-        this.position      = [randomX, randomY]
-        return this.position
-    },
-    drawSingleStar: function() {
-        ctx.beginPath()
-        ctx.arc(this.position[0], this.position[1], 2, 0, Math.PI * 2);
-        ctx.fill()
-        ctx.closePath()
-    },
-    starMotion: function() {
-        if(this.position[0] < 1680 || this.position[1] < 1024) {
-        this.position[0] += num
-        this.position[1] += num
-        } else {
-            this.position[0] -= num
-            this.position[1] -= num
+let randomYVector = function() {
+    let yNumber = Math.floor(Math.random() * 10) - 5 
+    return yNumber
+}
+let vectorY = randomYVector()
+
+// This class is a star. It is generating star's coordinate and vector, and drawing it. 
+// class Star  {
+//     constructor(){
+//         this.vectorX = randomXVector()
+//         this.vectorY = randomYVector()
+//         this.generatePosition()
+//         this.drawingSingleStar()
+//         this.moveStar()
+//         while(this.position[0] < CANVAS.width || this.position[1] < CANVAS.height) {
+//             this.moveStar()
+//             ctx.clearRect(0, 0, CANVAS.width, CANVAS.height);
+//             this.drawingSingleStar()
+//         }
+//     }
+//     generatePosition() {
+//         let randomX       = Math.floor(Math.random() * CANVAS.width);
+//         let randomY       = Math.floor(Math.random() * CANVAS.height);
+//         this.position     = [randomX, randomY]
+//         return this.position
+//     }
+//     drawingSingleStar() {
+//         ctx.beginPath()
+//         ctx.arc(this.position[0], this.position[1], 2, 0, Math.PI * 2);
+//         ctx.fill()
+//         ctx.closePath()
+//     }
+//     moveStar() {
+//         if(this.position[0] < CANVAS.width || this.position[1] < CANVAS.height) {
+//         this.position[0] += vectorX
+//         this.position[1] += vectorY
+//         } else {
+//             this.position[0] -= vectorX
+//             this.position[1] -= vectorY
+//         }
+//     }
+
+// };
+
+class Star { //this class desribes properties of star
+    constructor() {
+        this.starXPosition()
+        this.starYPosition()
+        this.generateXYvector()
+        while(true) {
+        this.starXvector()
+        this.starYvector()
         }
-    },
-
+    }
+    starXPosition() { // this method generates x  start position
+        this.xPosition = Math.floor(Math.random() * window.innerHeight);
+        return this.xPosition
+    }
+    starYPosition() { // this method generates y start position
+        this.yPosition = Math.floor(Math.random() * window.innerWidth);
+        return this.yPosition
+    }
+    generateXYvector() { // this method generates random number from -5 to 5 for the star's motion vector
+        this.randomXVector = function() {
+            this.xNumber = Math.floor(Math.random() * 10) - 5 
+            return this.xNumber
+    }
+    this.vectorX = this.randomXVector()
+    
+        this.randomYVector = function() {
+            this.yNumber = Math.floor(Math.random() * 10) - 5 
+            return this.yNumber
+    }
+     this.vectorY = this.randomYVector()
+    }
+    starXvector() { // this method changes x position
+        this.xPosition += this.xNumber
+    }
+    starYvector() { // this method changes y position
+        this.yPosition += this.yNumber
+    }
 };
